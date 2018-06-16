8338a635375616ee80d89d4d4e7d85620f5eec67
OWNER 0613318930 <dominiquekitwa2@gmail.com>
2018-06-16 21:39:08 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$("#ConnecterId").off("click");
+$("#ConnecterId").on("click", function(){
+    window.alert("Cette fonctionalité n'est pas encore operationnelle!");
+});
+
+$("#EnregistrerId").off("click");
+$("#EnregistrerId").on("click", function(){
+    window.alert("Cette fonctionalité n'est pas encore operationnelle!");
+});
+
+$("#RegisterId").off("click");
+$("#RegisterId").on("click", function(){
+    window.alert("Oops, This Feature is not ready yet!");
+});
+
+$("#SignInId").off("click");
+$("#SignInId").on("click", function(){
+    window.alert("Oops, This Feature is not ready yet!");
+});
+
+$("#IngiaId").off("click");
+$("#IngiaId").on("click", function(){
+    window.alert("Kipengele hiki hakiko tayari");
+});
+
+$("#KujiandikishaId").off("click");
+$("#KujiandikishaId").on("click", function(){
+    window.alert("Kipengele hiki hakiko tayari");
+});
+
+$("#KujiandikishaId").off("click");
+$("#KujiandikishaId").on("click", function(){
+    window.alert("Kipengele hiki hakiko tayari");
+});
+
+$("#LingalaId").off("click");
+$("#LingalaId").on("click", function(){
+    window.alert("Esaleli nanu te");
+});
+
+$("#LingalaIdEn").off("click");
+$("#LingalaIdEn").on("click", function(){
+    window.alert("Esaleli nanu te");
+});
+
+$("#LingalaIdSw").off("click");
+$("#LingalaIdSw").on("click", function(){
+    window.alert("Esaleli nanu te");
+});
+
+
+
+
